2d40b8fd6ab268b56fdc2ba8d2898de852844101
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-05-06 20:26:33 -0300

    updt: moved cards components to cards folder

--- src/app/pokedex.tsx
+++ src/app/pokedex.tsx
@@ -1,11 +1,11 @@
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context'; 
 import Navbar from '../components/navbar/Navbar';
-import CardPokemonGiratina from '../components/CardPokemonGiratina';
+import CardPokemonGiratina from '../components/cards/CardPokemonGiratina';
 import { ScrollView, Text } from 'react-native';
-import CardPokemonPalkia from '../components/CardPokemonPalkia';
-import CardPokemonDialga from '../components/CardPokemonDialga';
-import CardPokemonMewTwo from '../components/CardPokemonMewTwo';
-import CardPokemonZekrom from '../components/CardPokemonZekrom';
+import CardPokemonPalkia from '../components/cards/CardPokemonPalkia';
+import CardPokemonDialga from '../components/cards/CardPokemonDialga';
+import CardPokemonMewTwo from '../components/cards/CardPokemonMewTwo';
+import CardPokemonZekrom from '../components/cards/CardPokemonZekrom';
 
 export default function Pokedex () {
     return(
